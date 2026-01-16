00b25aeeccfa3dbce607de40452675968c8bf572
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-16 22:22:54 +0100

    update

--- src/main/java/com/example/ecommerce/config/ApplicationConfig.java
+++ src/main/java/com/example/ecommerce/config/ApplicationConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,6 +30,7 @@ public class ApplicationConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setUserDetailsPasswordService((UserDetailsPasswordService) userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
