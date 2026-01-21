cd287e3767d88348446e9d9b1dd101096a8a6376
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-21 12:07:03 +0100

    update

--- src/main/java/com/example/ecommerce/auth/AuthenticationController.java
+++ src/main/java/com/example/ecommerce/auth/AuthenticationController.java
@@ -20,7 +20,7 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authenticationService.authenticate(request));
