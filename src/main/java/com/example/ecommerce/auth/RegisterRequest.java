1f541a24ac40022133bf604bbc812eba3263f6e9
muhammed sule dabban <muhammedsulai4@gmail.com>
2026-01-22 12:29:58 +0100

    update

--- src/main/java/com/example/ecommerce/auth/RegisterRequest.java
+++ src/main/java/com/example/ecommerce/auth/RegisterRequest.java
@@ -1,6 +1,9 @@
 package com.example.ecommerce.auth;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +14,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
-
+    @NotBlank(message = "name can not be empty")
     private String name;
+
+    @NotBlank(message = "email can not be empty")
+    @Email(message = "Please provide a valid email address")
     private String email;
+
+    @NotBlank(message = "password must not be empty")
+    @Size(min = 4, message = "password size must not be < 4")
     private String password;
 }
